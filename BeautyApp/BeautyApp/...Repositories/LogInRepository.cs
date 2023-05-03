6b6bf35a13fe24c5c14dee086ff33445b6a392c6
TrifRebeca <rebecatrif000@gmail.com>
2023-05-03 18:10:54 +0300

    am adaugat fisierele lips

---
+++ BeautyApp/BeautyApp/...Repositories/LogInRepository.cs
@@ -0,0 +1,48 @@
+﻿using BeautyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyApp.Repositories
+{
+    class LogInRepository : BaseRepository, ILogInRepository
+    {
+        public LogInRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public void Add(ModelLogIn logInModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "insert into Admin values (@name, @password)";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = logInModel.Name;
+                command.Parameters.Add("@description", SqlDbType.NVarChar).Value = logInModel.Password;
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(ModelLogIn petModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ModelLogIn> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BeautyApp/BeautyApp/...Repositories/ProductRepository.cs
@@ -0,0 +1,121 @@
+﻿using BeautyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyApp.Repositories
+{
+    class ProductRepository : BaseRepository, IProductRepository
+    {
+        public ProductRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        //Methods
+        public IEnumerable<ProductModel> GetAll()
+        {
+            var productList = new List<ProductModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "Select *from Product order by Product_Id desc";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productModel = new ProductModel();
+                        productModel.Id = (int)reader[0];
+                        productModel.Name = reader[1].ToString();
+                        productModel.Description = reader[2].ToString();
+                        productModel.Price = reader[3].ToString();
+                        productModel.Ammount = reader[4].ToString();
+                        productList.Add(productModel);
+                    }
+                }
+            }
+            return productList;
+        }
+        public IEnumerable<ProductModel> GetByValue(string value)
+        {
+            var petList = new List<ProductModel>();
+            int petId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string petName = value;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"Select *from Product
+                                        where Product_Id=@id or Product_Name like @name+'%' 
+                                        order by Product_Id desc";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = petId;
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = petName;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var petModel = new ProductModel();
+                        petModel.Id = (int)reader[0];
+                        petModel.Name = reader[1].ToString();
+                        petModel.Description = reader[2].ToString();
+                        petModel.Price = reader[3].ToString();
+                        petModel.Ammount = reader[4].ToString();
+                        petList.Add(petModel);
+                    }
+                }
+            }
+            return petList;
+        }
+        public void Add(ProductModel productModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "insert into Product values (@name, @description, @price,@ammount)";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = productModel.Name;
+                command.Parameters.Add("@description", SqlDbType.NVarChar).Value = productModel.Description;
+                command.Parameters.Add("@price", SqlDbType.NVarChar).Value = productModel.Price;
+                command.Parameters.Add("@ammount", SqlDbType.NVarChar).Value = productModel.Ammount;
+                command.ExecuteNonQuery();
+            }
+        }
+        public void Delete(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "delete from Product where Product_Id=@id";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                command.ExecuteNonQuery();
+            }
+        }
+        public void Edit(ProductModel productModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"update Product 
+                                    set Product_Name=@name,Product_Descriere= @description,Product_Pret= @price,Product_Cantitate= @ammount
+                                    where Product_Id=@id";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = productModel.Name;
+                command.Parameters.Add("@description", SqlDbType.NVarChar).Value = productModel.Description;
+                command.Parameters.Add("@price", SqlDbType.NVarChar).Value = productModel.Price;
+                command.Parameters.Add("@ammount", SqlDbType.NVarChar).Value = productModel.Ammount;
+                command.Parameters.Add("@id", SqlDbType.Int).Value = productModel.Id;
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
